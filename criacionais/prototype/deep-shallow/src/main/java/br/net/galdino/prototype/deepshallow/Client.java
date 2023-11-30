c7a8aa2a8587491fd282cd0451a97b33f9c1b9bc
“Wagner” <wagalwagal@gmail.com>
2023-11-30 17:28:31 -0300

    Exercícios de Prototype Design Pattern

---
+++ criacionais/prototype/deep-shallow/src/main/java/br/net/galdino/prototype/deepshallow/Client.java
@@ -0,0 +1,18 @@
+package br.net.galdino.prototype.deepshallow;
+
+import br.net.galdino.prototype.deepshallow.model.Address;
+import br.net.galdino.prototype.deepshallow.model.User;
+
+public class Client {
+
+	public static void main(String[] args) throws CloneNotSupportedException {
+		User user = new User("Clone", 25, new Address("ABC Street", 1000));
+		System.out.println(user);
+		
+		User cloneUser = user.clone();
+		cloneUser.name = "Clone x2";
+		cloneUser.address.street = "Double Street";
+		System.out.println(user);
+		System.out.println(cloneUser);
+	}
+}
